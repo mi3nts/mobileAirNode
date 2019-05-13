18b5aca47b9bfc3934dcfc7b4f6e25b0212e7ae9
Lakitha Omal Harindha Wijeratne <lhw150030@utdallas.edu>
2019-05-13 14:07:48 -0500

    Update runAll.sh

--- firmware/xu4/runAll.sh
+++ firmware/xu4/runAll.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 #
-sleep 60
+sleep 30
 python3 nanoReader0.py &
 sleep 5
 python3 nanoReader1.py &
-sleep 5
-python3 nanoReader2.py &
-sleep 5
-python3 GPSReader.py &
+# sleep 5
+# python3 nanoReader2.py &
+# sleep 5
+# python3 GPSReader.py &
 sleep 5
 python3 ipReader.py
-
-# python3 centralNodeReaderNano.py &
-# sleep 10
-# cd odroidShow2 && python3 mintsShow2.py
+sleep 10
+cd odroidShow2 && python3 mintsShow2.py
