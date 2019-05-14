8c07c4c7d06f769476b1b843a64e7976c0f49e20
Teamlary <lhw150030@utdallas.edu>
2019-05-14 13:27:45 -0500

    daily Updates

--- firmware/xu4/thermalCamReader.py
+++ firmware/xu4/thermalCamReader.py
@@ -89,6 +89,8 @@ def main():
                 if((time.time()-startTime)>10):
                     startTime = time.time()
                     dataCelcius           = ktoc(dataKelvin)
+                    print(dataCelcius)
+                    print(dataCelcius)
                     dataCelciusMultiplied = kelvinToCelcius(dataKelvin)
                     minCelcius, maxCelcius, minLocation, maxLocation = cv2.minMaxLoc(dataCelcius)
                     sensorDictionary =  OrderedDict([
